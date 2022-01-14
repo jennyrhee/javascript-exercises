1a522cb4728d2f62f72c154540aa10d344bc5fa3
jennyrhee <jennyirhee@gmail.com>
2022-01-14 11:19:04 -0600

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(intA, intB) {
+  if (intA < 0 || intB < 0 || typeof intA !== 'number' || typeof intB !== 'number') {
+    return 'ERROR';
+  }
 
+  let sum = 0;
+  let smallest = Math.min(intA, intB)
+  let largest = Math.max(intA, intB)
+  for (let i = smallest; i <= largest; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
