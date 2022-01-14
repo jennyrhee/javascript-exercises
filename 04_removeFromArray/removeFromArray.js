2577583061252ecfa0158b48383c459ff15486d6
jennyrhee <jennyirhee@gmail.com>
2022-01-14 11:09:54 -0600

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+  let newArray = [...array]
+  for (const element of elementsToRemove) {
+    let index = newArray.indexOf(element);
+    if (index !== -1) {
+      newArray.splice(index, 1);
+    }
+  }
+  return newArray; 
 };
 
 // Do not edit below this line
