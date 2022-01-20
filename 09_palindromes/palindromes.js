fcdece0810c3b13fbad55202730c5c31b705e34e
jennyrhee <jennyirhee@gmail.com>
2022-01-20 15:16:37 -0600

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let newString = string.toLowerCase().replaceAll(/[^\w]/g, '');
+  let reversed = newString
+                  .split('')
+                  .reverse()
+                  .toString()
+                  .replaceAll(',', '');
+  return newString === reversed;
 };
 
 // Do not edit below this line
